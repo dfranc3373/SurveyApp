0b35414daaef4797d2551ba7b8dc3a1d29a2ef85
xartemx <adtest78@gmail.com>
2015-03-11 14:48:51 -0500

    Added notNull unit tests for models

--- android/SurveyApp_I1_v2/app/src/test/java/Models/CouponTest.java
+++ android/SurveyApp_I1_v2/app/src/test/java/Models/CouponTest.java
@@ -1,5 +1,20 @@
 package Models;
 
-public class CouponTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import static junit.framework.Assert.assertNotNull;
+
+public class CouponTest{
+
+    @Before
+    public void setup(){
+        //do setup here
+    }
+
+    @Test
+    public void notNull(){
+        Coupon coupon = new Coupon();
+        assertNotNull(coupon);
+    }
 }
--- android/SurveyApp_I1_v2/app/src/test/java/Models/SurveyTest.java
+++ android/SurveyApp_I1_v2/app/src/test/java/Models/SurveyTest.java
@@ -1,5 +1,16 @@
 package Models;
 
+import org.junit.Test;
+
+import static junit.framework.Assert.assertNotNull;
+
 public class SurveyTest {
+
+    @Test
+    public void notNull(){
+
+        Survey surv = new Survey();
+        assertNotNull(surv);
+    }
 
 }
