3491e91c8f8f0e760ddaae586feaa12ff76dfafb
xartemx <adtest78@gmail.com>
2015-04-18 12:14:48 -0500

    Working on RateSurvey activity layout

--- android/SurveyApp_I1_v2/app/src/main/java/com/example/macbook/surveyapp_i1/RateSurvey.java
+++ android/SurveyApp_I1_v2/app/src/main/java/com/example/macbook/surveyapp_i1/RateSurvey.java
@@ -1,39 +1,43 @@
 package com.example.macbook.surveyapp_i1;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class RateSurvey extends ActionBarActivity {
+
+    ImageButton btnRateOne;
+    ImageButton btnRateTwo;
+    ImageButton btnRateThree;
+    ImageButton btnRateFour;
+    ImageButton btnSubmitRating;
+
+    TextView tvSkipRating;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rate_survey);
+
+        btnRateOne = (ImageButton) findViewById(R.id.btnStarOne);
+        btnRateTwo = (ImageButton) findViewById(R.id.btnStarTwo);
+        btnRateThree = (ImageButton) findViewById(R.id.btnStarThree);
+        btnRateFour = (ImageButton) findViewById(R.id.btnStarFour);
+        btnSubmitRating = (ImageButton) findViewById(R.id.btnSubmitRating);
+
+        tvSkipRating = (TextView) findViewById(R.id.tvSkipRating);
+
+        btnRateOne.setImageResource(R.drawable.grey_star);
+        btnRateTwo.setImageResource(R.drawable.grey_star);
+        btnRateThree.setImageResource(R.drawable.grey_star);
+        btnRateFour.setImageResource(R.drawable.grey_star);
+
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_rate_survey, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
