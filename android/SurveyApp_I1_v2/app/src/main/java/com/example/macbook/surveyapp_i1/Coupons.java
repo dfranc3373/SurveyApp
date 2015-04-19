82a5b18e670ff3f4682e2bd3d355fbedeb8b4826
Sandesh <sandesh.bhattarai@ttu.edu>
2015-04-19 12:45:46 -0500

    coupons class added.
    coupons class added.

---
+++ android/SurveyApp_I1_v2/app/src/main/java/com/example/macbook/surveyapp_i1/Coupons.java
@@ -0,0 +1,20 @@
+package com.example.macbook.surveyapp_i1;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Coupons extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coupons);
+    }
+
+
+
+}
