9a96f0447083e3b9fbdba65a7ff7c1f2afbc9166
Sandesh <sandesh.bhattarai@ttu.edu>
2015-05-03 14:49:28 -0500

    LOGIN updated
    LOGIN updated

---
+++ android/SurveyApp_I1_v2/app/src/main/java/com/example/macbook/surveyapp_i1/Login.java
@@ -0,0 +1,17 @@
+package com.example.macbook.surveyapp_i1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by macbookuser on 5/3/15.
+ */
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
